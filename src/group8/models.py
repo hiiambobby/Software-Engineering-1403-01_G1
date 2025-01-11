363fb516c8d43633680ec20dfbdd4a75e7413423
Parand1Khalili <parandkhalili@aut.ac.ir>
2025-01-11 20:32:12 +0330

    initial progress report added

--- src/group8/models.py
+++ src/group8/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Level(models.Model):
+    name = models.CharField(max_length=100)
+
+class Word(models.Model):
+    word = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    learned_users = models.ManyToManyField(User, related_name='learned_words')
+
+    def __str__(self):
+        return self.word
--- src/group8/views.py
+++ src/group8/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Word, Category, Level
+from django.db.models import Count
+from django.http import HttpResponse
+
+
 
 # Create your views here.
 
 def home(request):
     return render (request , 'group8.html' , {'group_number': '8'})
 
+@login_required
+def ProgressReport(request):
+    user = request.user
+
+    # Count total words learned by the user
+    total_learned = user.learned_words.count()
+
+    # Count words learned per category
+    category_progress = Category.objects.annotate(
+        learned_words_count=Count('word__learned_users')
+    )
+
+    # Count words learned per level
+    level_progress = Level.objects.annotate(
+        learned_words_count=Count('word__learned_users')
+    )
+
+    context = {
+        'total_learned': total_learned,
+        'category_progress': category_progress,
+        'level_progress': level_progress,
+    }
+
+    return render(request, 'progress_report.html', context)
+
+
+@login_required
+def MarkAsLearned(request, word_id):
+    word = get_object_or_404(Word, id=word_id)
+
+    # Add the user to the list of learned users for this word
+    word.learned_users.add(request.user)
+
+    return HttpResponse("Word marked as learned!")
