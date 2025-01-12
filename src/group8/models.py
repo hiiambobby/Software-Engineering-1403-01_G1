bb6fa5a5dd8f36315c431bad4c46ff8a70ad2a57
Parand1Khalili <parandkhalili@aut.ac.ir>
2025-01-12 18:15:15 +0330

    adding manage words initial

--- src/group8/models.py
+++ src/group8/models.py
@@ -1,17 +1,43 @@
-from django.db import models
 from django.contrib.auth.models import User
-
-class Category(models.Model):
-    name = models.CharField(max_length=100)
-
-class Level(models.Model):
-    name = models.CharField(max_length=100)
+from django.db import models
 
 class Word(models.Model):
-    word = models.CharField(max_length=100)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    level = models.ForeignKey(Level, on_delete=models.CASCADE)
-    learned_users = models.ManyToManyField(User, related_name='learned_words')
+
+    title = models.CharField(max_length=255,default="none")
+    level = models.CharField(max_length=50,default="none")
+    category = models.CharField(max_length=50, default="none")
+    image_url = models.CharField(max_length=255, default="http://example.com/default_image.jpg")
+
+    def get_category(self):
+        return self.category
+
+    def get_image_url(self):
+        return self.image_url
+
+    def get_level(self):
+        return self.level
+
+    def get_title(self):
+        return self.title
 
     def __str__(self):
-        return self.word
+        return self.title
+    
+class UserProgress(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='progress')
+    learned_words = models.ManyToManyField(Word, related_name='learned_by')
+
+    def get_total_learned(self):
+        return self.learned_words.count()
+
+    def get_learned_by_category(self):
+        return self.learned_words.values('category').annotate(count=models.Count('category'))
+
+    def get_learned_by_level(self):
+        return self.learned_words.values('level').annotate(count=models.Count('level'))
+
+    def __str__(self):
+        return f"Progress for {self.user.username}"
+
+########################################karbala   : done 
+
