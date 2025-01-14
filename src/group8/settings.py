f79d7bb3d52c2aab2f92a781861cbaa91c2096bf
hiiiambobby <saba3st@gmail.com>
2025-01-14 19:14:54 +0330

    cloud database

---
+++ src/group8/settings.py
@@ -0,0 +1,14 @@
+from database.secret import DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',  # or 'django.db.backends.postgresql', etc.
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'PASSWORD': DB_PASSWORD,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT,
+    }
+}
+
+
